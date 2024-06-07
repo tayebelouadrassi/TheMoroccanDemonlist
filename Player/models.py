cdb5cb448f5f25424452890f07d487cb6d5128ee
apostorial <t.elouadrassi@gmail.com>
2024-06-07 12:00:56 +0100

    Added player model.

--- Player/models.py
+++ Player/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+class PlayerManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class Player(AbstractBaseUser, PermissionsMixin):
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    classic_points = models.FloatField(default=0)
+    platformer_points = models.FloatField(default=0)
+
+    USERNAME_FIELD = 'username'
+
+    objects = PlayerManager()
+
+    def __str__(self):
+        return self.username
